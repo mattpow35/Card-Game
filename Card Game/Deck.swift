4d5db10e3fb078a9fcd29c2cc6bbbe43353381bb
mattpow35 <mpow2231@740s-pl-2.local>
2016-10-31 08:11:12 -0600

    added the deck class to begin making the deck for game

---
+++ Card Game/Deck.swift
@@ -0,0 +1,22 @@
+//
+//  Deck.swift
+//  Card Game
+//
+//  Created by Powley, Matthew on 10/31/16.
+//  Copyright © 2016 Powley, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class Deck : PlayingCard
+
+{
+    
+    
+    
+    
+    
+    
+    
+    
+}
