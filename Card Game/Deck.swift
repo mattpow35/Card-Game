d6be8336c40ec97ae60bc6a05946828eb5f905e9
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-09 08:31:12 -0700

    added the shuffle deck method

--- Card Game/Deck.swift
+++ Card Game/Deck.swift
@@ -14,7 +14,22 @@ class Deck
     
     func shuffleDeck() -> Void
     {
+        var tempDeck = [Card]()
+        while self.cards.count > 0
+        {
+            //get a random #
+            //chooses a valid random index in the range of 0 ... cards.count-1
+            let randomSpot = Int (arc4random() % UInt32(cards.count))
+            //get that card from cards
+            //takes the card from that spot from the deck, The deck decreases in count
+            let removeCard = cards.removeAtIndex(randomSpot)
+            //put that card in temp
+            //add the removed card to the end of the temporary deck.
+            tempDeck.append(removeCard)
         
+        }
+        //replace the original deck with the temp deck that was shuffled.
+        cards = tempDeck
     }
     
     func cutDeck() -> Void
